e2be21109e6d64e3c829b1470d00d38bd0b9b4b9
Nick <82282004+nickgeiserr@users.noreply.github.com>
2024-03-18 21:23:58 -0400

    got it working

--- stores/user.go
+++ stores/user.go
@@ -1,10 +1,17 @@
 package stores
 
-import "NeplodeAPI/models"
+import (
+	"NeplodeAPI/database"
+	"NeplodeAPI/logger"
+	"NeplodeAPI/models"
+	"context"
+	"go.uber.org/zap"
+)
 
 type (
 	UserStore interface {
 		GetAll() ([]models.User, error)
+		CreateUser(newUser models.User) bool
 	}
 
 	userStore struct {
@@ -12,5 +19,27 @@ type (
 )
 
 func (u userStore) GetAll() ([]models.User, error) {
+	var name string
+	err := database.DB.QueryRow(context.Background(), "select 'Nick'").Scan(&name)
+	if err != nil {
+		return nil, err
+	}
 	return make([]models.User, 0), nil
+}
+
+func (u userStore) CreateUser(newUser models.User) bool {
+
+	query := "INSERT INTO users (id, username, bio, birthday, profile_picture) VALUES ($1, $2, $3, $4, $5)"
+
+	if database.DB == nil {
+		logger.Fatal("sum went wrong")
+	}
+
+	row, err := database.DB.Query(context.Background(), query, newUser.ID, newUser.Username, newUser.Bio, newUser.Birthday, newUser.ProfilePicture)
+	if err != nil {
+		logger.Error("Something went wrong : ", zap.Error(err))
+		return false
+	}
+	row.Close()
+	return true
 }
